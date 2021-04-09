11caac929c5994a2ddde33b8b5bef7f656a82496
RAJIT-018 <71140719+RAJIT-018@users.noreply.github.com>
2021-04-09 12:10:21 +0530

    Add files via upload
    ##This cpp file can print many amazing patterns in the console based on dimensions you can specify!

---
+++ main.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int rowCount;
+    int colCount;
+
+    cout << "Enter the amount of rows: ";
+    cin >> rowCount;
+    cout << "Enter the amount of columns: ";
+    cin >> colCount;
+//Rectangle
+    cout << "RECTANGLE" << endl;
+    for (int i = 0; i < rowCount; ++i) {
+        for (int j = 0; j < colCount; ++j) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    cout << "Enter the amount of rows: ";
+    cin >> rowCount;
+    cout << "Enter the amount of columns: ";
+    cin >> colCount;
+//Hollow Rectangle
+    cout << "Hollow Rectangle" << endl;
+    for (int i = 0; i < rowCount; ++i) {
+        if (i == 0 || i == rowCount - 1) {
+            for (int j = 0; j < colCount; ++j) {
+                cout << "* ";
+            }
+            cout << endl;
+        } else {
+            for (int j = 0; j < colCount; ++j) {
+                if (j == 0 || j == colCount - 1) {
+                    cout << "* ";
+                } else {
+                    cout << "  ";
+                }
+            }
+            cout << endl;
+        }
+    }
+
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+// inverted half pyramid
+    cout << "Inverted Half Pyramid" << endl;
+    for (int i = n; i >= 1; i--) {
+        for (int j = 0; j < i; ++j) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    cout << "Enter n: ";
+    cin >> n;
+//Half pyramid inverted horizontally
+    cout << "Half Pyramid Inverted Horizontally" << endl;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n - i; ++j) {
+            cout << "  ";
+        }
+        for (int k = 0; k < i; ++k) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    cout << "Enter n: ";
+    cin >> n;
+//Repeated Num Sequence
+    cout << "Repeated Num Sequence" << endl;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "Enter n: ";
+    cin >> n;
+//Num Sequence
+    cout << "Num Sequence" << endl;
+    int numCount = 0;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            numCount++;
+            cout << numCount << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "Enter n: ";
+    cin >> n;
+//Butterfly pattern
+    cout << "Butterfly Pattern" << endl;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            cout << "* ";
+        }
+        for (int k = 0; k < (n * 2) - (i * 2); ++k) {
+            cout << "  ";
+        }
+        for (int j = 0; j < i; ++j) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for (int i = n; i >= 1; --i) {
+        for (int j = 0; j < i; ++j) {
+            cout << "* ";
+        }
+        for (int k = 0; k < (n * 2) - (i * 2); ++k) {
+            cout << "  ";
+        }
+        for (int j = 0; j < i; ++j) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
